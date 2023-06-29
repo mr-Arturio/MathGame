20e8018529b31360daf64be28337b789f5245ce5
ArturT <mr.arturio@gmail.com>
2023-06-28 22:15:03 -0400

    add code for: switch player and end game

--- Game.rb
+++ Game.rb
@@ -4,54 +4,71 @@ require "./Question.rb"
 
 # main logic of the game
 class Game
-  def initialize
-    @players = []
-
-    @current_player = @players.first
+  def initialize(player1_name, player2_name)
+    @players = [Player.new(player1_name), Player.new(player2_name)]
+    @player_index = 0
+    @current_player = @players[0]
   end
 
   def start_game
-    puts "--- New Turn ---"
-    question = generate_question
-    puts "#{current_player.name}, here's your question: #{question.generate_question_text}"
-    answer = gets.chomp.to_i
+    loop do
+      puts "--- New Turn ---"
+      question = ask_question
+      puts "#{@current_player.name}, here's your question: #{question.generate_question_text}"
+      answer = gets.chomp.to_i
+
+      if question.check_answer(answer)
+        puts "Correct!"
+      else
+        puts "Wrong!!!"
+        @current_player.decrease_lives
+      end
+
+      display_scores
+
+      #he ? at the end of the method name conventionally indicates that the method returns a Boolean value
+      if game_over?
+        break
+      end
+
+      # switch_player
+      if @player_index == 0
+       @player_index = 1
+      else 
+        @player_index = 0
+      end
+
+      @current_player = @players[@player_index]
+        
+
+
+      # def announce_winner
+      
+    end
   end
 
   def ask_question
     Question.new
   end
 
-  # def display_score
+  def display_scores
+    @players.each do |player|
+      puts "#{player.name}:Lives - #{player.lives}/3"
+    end
+  end
 
-  # end
+  def game_over? # '?' indicates that the method returns a Boolean value
+    # 'any?' returns true if at least one element in the collection satisfies the condition
+   @players.any? { |player| player.lives <= 0 }
+     end
 
-  # def switch_player
 
-  # end
-
-  # def game_over
-
-  # end
-
+  
   # def announce_winner
 
   # end
 end
 
-=begin
-1. ask question for player1
-   - question from Question file 
-   - user answer gets.chomp
-   - we need to check if the answer is correct
-   - if not decrease lives
-   -display score for both
-2. switch player
-3.another player logic same as p1
 
-4. logic for the end gtame if lives less than 1
-5. anounce winner
-
-=end
-
-game = Game.new
+game = Game.new("Player 1", "Player 2")
 game.start_game
